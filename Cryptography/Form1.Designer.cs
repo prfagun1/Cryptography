f5a8d4b32911e7614d7c13f9f897902125f103b2
Pablo Rogério Fagundes <prfagun1@hotmail.com>
2020-02-24 20:18:47 -0300

    First version

---
+++ Cryptography/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class fCriptografiaContingenciaAutorizador
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bDescriptografar = new System.Windows.Forms.Button();
+            this.tTextoCriptografado02 = new System.Windows.Forms.TextBox();
+            this.tChaveDeDescriptografia = new System.Windows.Forms.TextBox();
+            this.tTexto02 = new System.Windows.Forms.TextBox();
+            this.lDescriptografar = new System.Windows.Forms.Label();
+            this.lCriptografar = new System.Windows.Forms.Label();
+            this.lTexto01 = new System.Windows.Forms.Label();
+            this.tTexto01 = new System.Windows.Forms.TextBox();
+            this.lChaveDeCriptografia = new System.Windows.Forms.Label();
+            this.tChaveDeCriptografia = new System.Windows.Forms.TextBox();
+            this.tTextoCriptografado01 = new System.Windows.Forms.TextBox();
+            this.lTextoCriptografado01 = new System.Windows.Forms.Label();
+            this.lTextoCriptografado02 = new System.Windows.Forms.Label();
+            this.lChaveDeDescriptografia = new System.Windows.Forms.Label();
+            this.lTexto02 = new System.Windows.Forms.Label();
+            this.bCriptografar = new System.Windows.Forms.Button();
+            this.bCopiar01 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.bGerarHash = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bDescriptografar
+            // 
+            this.bDescriptografar.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bDescriptografar.Location = new System.Drawing.Point(618, 492);
+            this.bDescriptografar.Name = "bDescriptografar";
+            this.bDescriptografar.Size = new System.Drawing.Size(131, 33);
+            this.bDescriptografar.TabIndex = 0;
+            this.bDescriptografar.Text = "Descriptografar";
+            this.bDescriptografar.UseVisualStyleBackColor = true;
+            this.bDescriptografar.Click += new System.EventHandler(this.bDescriptografar_Click);
+            // 
+            // tTextoCriptografado02
+            // 
+            this.tTextoCriptografado02.Location = new System.Drawing.Point(618, 89);
+            this.tTextoCriptografado02.Multiline = true;
+            this.tTextoCriptografado02.Name = "tTextoCriptografado02";
+            this.tTextoCriptografado02.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tTextoCriptografado02.Size = new System.Drawing.Size(549, 135);
+            this.tTextoCriptografado02.TabIndex = 1;
+            // 
+            // tChaveDeDescriptografia
+            // 
+            this.tChaveDeDescriptografia.Location = new System.Drawing.Point(618, 260);
+            this.tChaveDeDescriptografia.Name = "tChaveDeDescriptografia";
+            this.tChaveDeDescriptografia.Size = new System.Drawing.Size(549, 20);
+            this.tChaveDeDescriptografia.TabIndex = 2;
+            // 
+            // tTexto02
+            // 
+            this.tTexto02.Location = new System.Drawing.Point(618, 322);
+            this.tTexto02.Multiline = true;
+            this.tTexto02.Name = "tTexto02";
+            this.tTexto02.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tTexto02.Size = new System.Drawing.Size(549, 152);
+            this.tTexto02.TabIndex = 3;
+            // 
+            // lDescriptografar
+            // 
+            this.lDescriptografar.AutoSize = true;
+            this.lDescriptografar.Font = new System.Drawing.Font("Trebuchet MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lDescriptografar.Location = new System.Drawing.Point(613, 25);
+            this.lDescriptografar.Name = "lDescriptografar";
+            this.lDescriptografar.Size = new System.Drawing.Size(160, 27);
+            this.lDescriptografar.TabIndex = 4;
+            this.lDescriptografar.Text = "Descriptografar";
+            // 
+            // lCriptografar
+            // 
+            this.lCriptografar.AutoSize = true;
+            this.lCriptografar.Font = new System.Drawing.Font("Trebuchet MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lCriptografar.Location = new System.Drawing.Point(12, 25);
+            this.lCriptografar.Name = "lCriptografar";
+            this.lCriptografar.Size = new System.Drawing.Size(130, 27);
+            this.lCriptografar.TabIndex = 5;
+            this.lCriptografar.Text = "Criptografar";
+            // 
+            // lTexto01
+            // 
+            this.lTexto01.AutoSize = true;
+            this.lTexto01.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lTexto01.Location = new System.Drawing.Point(17, 68);
+            this.lTexto01.Name = "lTexto01";
+            this.lTexto01.Size = new System.Drawing.Size(40, 18);
+            this.lTexto01.TabIndex = 6;
+            this.lTexto01.Text = "Texto";
+            // 
+            // tTexto01
+            // 
+            this.tTexto01.Location = new System.Drawing.Point(20, 89);
+            this.tTexto01.Multiline = true;
+            this.tTexto01.Name = "tTexto01";
+            this.tTexto01.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tTexto01.Size = new System.Drawing.Size(549, 135);
+            this.tTexto01.TabIndex = 7;
+            // 
+            // lChaveDeCriptografia
+            // 
+            this.lChaveDeCriptografia.AutoSize = true;
+            this.lChaveDeCriptografia.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lChaveDeCriptografia.Location = new System.Drawing.Point(17, 239);
+            this.lChaveDeCriptografia.Name = "lChaveDeCriptografia";
+            this.lChaveDeCriptografia.Size = new System.Drawing.Size(137, 18);
+            this.lChaveDeCriptografia.TabIndex = 8;
+            this.lChaveDeCriptografia.Text = "Chave de Criptografia";
+            // 
+            // tChaveDeCriptografia
+            // 
+            this.tChaveDeCriptografia.Location = new System.Drawing.Point(17, 260);
+            this.tChaveDeCriptografia.Name = "tChaveDeCriptografia";
+            this.tChaveDeCriptografia.Size = new System.Drawing.Size(549, 20);
+            this.tChaveDeCriptografia.TabIndex = 9;
+            // 
+            // tTextoCriptografado01
+            // 
+            this.tTextoCriptografado01.Location = new System.Drawing.Point(17, 322);
+            this.tTextoCriptografado01.Multiline = true;
+            this.tTextoCriptografado01.Name = "tTextoCriptografado01";
+            this.tTextoCriptografado01.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tTextoCriptografado01.Size = new System.Drawing.Size(549, 152);
+            this.tTextoCriptografado01.TabIndex = 10;
+            // 
+            // lTextoCriptografado01
+            // 
+            this.lTextoCriptografado01.AutoSize = true;
+            this.lTextoCriptografado01.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lTextoCriptografado01.Location = new System.Drawing.Point(17, 301);
+            this.lTextoCriptografado01.Name = "lTextoCriptografado01";
+            this.lTextoCriptografado01.Size = new System.Drawing.Size(169, 18);
+            this.lTextoCriptografado01.TabIndex = 11;
+            this.lTextoCriptografado01.Text = "Texto Criptografado / Hash";
+            // 
+            // lTextoCriptografado02
+            // 
+            this.lTextoCriptografado02.AutoSize = true;
+            this.lTextoCriptografado02.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lTextoCriptografado02.Location = new System.Drawing.Point(615, 68);
+            this.lTextoCriptografado02.Name = "lTextoCriptografado02";
+            this.lTextoCriptografado02.Size = new System.Drawing.Size(125, 18);
+            this.lTextoCriptografado02.TabIndex = 12;
+            this.lTextoCriptografado02.Text = "Texto Criptografado";
+            // 
+            // lChaveDeDescriptografia
+            // 
+            this.lChaveDeDescriptografia.AutoSize = true;
+            this.lChaveDeDescriptografia.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lChaveDeDescriptografia.Location = new System.Drawing.Point(615, 239);
+            this.lChaveDeDescriptografia.Name = "lChaveDeDescriptografia";
+            this.lChaveDeDescriptografia.Size = new System.Drawing.Size(155, 18);
+            this.lChaveDeDescriptografia.TabIndex = 13;
+            this.lChaveDeDescriptografia.Text = "Chave de descriptografia";
+            // 
+            // lTexto02
+            // 
+            this.lTexto02.AutoSize = true;
+            this.lTexto02.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lTexto02.Location = new System.Drawing.Point(615, 301);
+            this.lTexto02.Name = "lTexto02";
+            this.lTexto02.Size = new System.Drawing.Size(40, 18);
+            this.lTexto02.TabIndex = 14;
+            this.lTexto02.Text = "Texto";
+            // 
+            // bCriptografar
+            // 
+            this.bCriptografar.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bCriptografar.Location = new System.Drawing.Point(20, 492);
+            this.bCriptografar.Name = "bCriptografar";
+            this.bCriptografar.Size = new System.Drawing.Size(122, 33);
+            this.bCriptografar.TabIndex = 15;
+            this.bCriptografar.Text = "Criptografar";
+            this.bCriptografar.UseVisualStyleBackColor = true;
+            this.bCriptografar.Click += new System.EventHandler(this.bCriptografar_Click);
+            // 
+            // bCopiar01
+            // 
+            this.bCopiar01.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bCopiar01.Location = new System.Drawing.Point(444, 492);
+            this.bCopiar01.Name = "bCopiar01";
+            this.bCopiar01.Size = new System.Drawing.Size(122, 33);
+            this.bCopiar01.TabIndex = 16;
+            this.bCopiar01.Text = "Copiar";
+            this.bCopiar01.UseVisualStyleBackColor = true;
+            this.bCopiar01.Click += new System.EventHandler(this.bCopiar01_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(1045, 492);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 33);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Copiar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // bGerarHash
+            // 
+            this.bGerarHash.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bGerarHash.Location = new System.Drawing.Point(229, 492);
+            this.bGerarHash.Name = "bGerarHash";
+            this.bGerarHash.Size = new System.Drawing.Size(122, 33);
+            this.bGerarHash.TabIndex = 18;
+            this.bGerarHash.Text = "Gerar Hash";
+            this.bGerarHash.UseVisualStyleBackColor = true;
+            this.bGerarHash.Click += new System.EventHandler(this.bGerarHash_Click);
+            // 
+            // fCriptografiaContingenciaAutorizador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1179, 537);
+            this.Controls.Add(this.bGerarHash);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.bCopiar01);
+            this.Controls.Add(this.bCriptografar);
+            this.Controls.Add(this.lTexto02);
+            this.Controls.Add(this.lChaveDeDescriptografia);
+            this.Controls.Add(this.lTextoCriptografado02);
+            this.Controls.Add(this.lTextoCriptografado01);
+            this.Controls.Add(this.tTextoCriptografado01);
+            this.Controls.Add(this.tChaveDeCriptografia);
+            this.Controls.Add(this.lChaveDeCriptografia);
+            this.Controls.Add(this.tTexto01);
+            this.Controls.Add(this.lTexto01);
+            this.Controls.Add(this.lCriptografar);
+            this.Controls.Add(this.lDescriptografar);
+            this.Controls.Add(this.tTexto02);
+            this.Controls.Add(this.tChaveDeDescriptografia);
+            this.Controls.Add(this.tTextoCriptografado02);
+            this.Controls.Add(this.bDescriptografar);
+            this.Name = "fCriptografiaContingenciaAutorizador";
+            this.Text = "Cryptography";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bDescriptografar;
+        private System.Windows.Forms.TextBox tTextoCriptografado02;
+        private System.Windows.Forms.TextBox tChaveDeDescriptografia;
+        private System.Windows.Forms.TextBox tTexto02;
+        private System.Windows.Forms.Label lDescriptografar;
+        private System.Windows.Forms.Label lCriptografar;
+        private System.Windows.Forms.Label lTexto01;
+        private System.Windows.Forms.TextBox tTexto01;
+        private System.Windows.Forms.Label lChaveDeCriptografia;
+        private System.Windows.Forms.TextBox tChaveDeCriptografia;
+        private System.Windows.Forms.TextBox tTextoCriptografado01;
+        private System.Windows.Forms.Label lTextoCriptografado01;
+        private System.Windows.Forms.Label lTextoCriptografado02;
+        private System.Windows.Forms.Label lChaveDeDescriptografia;
+        private System.Windows.Forms.Label lTexto02;
+        private System.Windows.Forms.Button bCriptografar;
+        private System.Windows.Forms.Button bCopiar01;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button bGerarHash;
+    }
+}
+
---
+++ Cryptography/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class fCriptografiaContingenciaAutorizador : Form
+    {
+        public fCriptografiaContingenciaAutorizador(){
+            InitializeComponent();
+        }
+
+
+        public string DecryptAES(string cipherText, string key){
+            string EncryptionKey = key;
+            byte[] cipherBytes = Convert.FromBase64String(cipherText);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (System.IO.MemoryStream ms = new System.IO.MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(cipherBytes, 0, cipherBytes.Length);
+                        cs.Close();
+                    }
+                    cipherText = Encoding.Unicode.GetString(ms.ToArray());
+                }
+            }
+            return cipherText;
+        }
+
+        public string EncryptAES(string clearText, string key) {
+            string EncryptionKey = key;
+            byte[] clearBytes = Encoding.Unicode.GetBytes(clearText);
+            using (Aes encryptor = Aes.Create()) {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (System.IO.MemoryStream ms = new System.IO.MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(clearBytes, 0, clearBytes.Length);
+                        cs.Close();
+                    }
+                    clearText = Convert.ToBase64String(ms.ToArray());
+                }
+            }
+            return clearText;
+        }
+
+
+        private void bDescriptografar_Click(object sender, EventArgs e){
+            try{
+                tTexto02.Text = this.DecryptAES(tTextoCriptografado02.Text, tChaveDeDescriptografia.Text);
+            }
+            catch(Exception erro) {
+                tTexto02.Text = "Erro ao descriptografar";
+            }
+
+
+        }
+
+        private void bCriptografar_Click(object sender, EventArgs e)  {
+            try { 
+                tTextoCriptografado01.Text = this.EncryptAES(tTexto01.Text, tChaveDeCriptografia.Text);
+            }
+            catch(Exception erro) {
+                tTexto01.Text = "Erro ao criptogragar: " + e.ToString();
+            }
+        }
+
+        private void bCopiar01_Click(object sender, EventArgs e){
+            Clipboard.SetText(tTextoCriptografado01.Text);
+        }
+
+        private void button1_Click(object sender, EventArgs e){
+            Clipboard.SetText(tTexto02.Text);
+        }
+
+        private void bGerarHash_Click(object sender, EventArgs e){
+            String texto = DateTime.Now.Hour.ToString() + DateTime.Now.Minute.ToString() + tChaveDeCriptografia.Text;
+
+            tTextoCriptografado01.Text = this.EncryptAES(texto, tChaveDeCriptografia.Text).Replace("/","");
+        }
+    }
+}
